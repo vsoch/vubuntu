12656420c2cac62b4273b1f253dae3b83d9dc6dd
vsoch <vsochat@stanford.edu>
2016-10-13 18:02:29 -0700

    adding install for databases, ruby, and jekyll

--- history.sh
+++ history.sh
@@ -56,8 +56,58 @@ wget https://repo.continuum.io/archive/Anaconda3-4.2.0-Linux-x86_64.sh
 bash Anaconda2-4.2.0-Linux-x86_64.sh -b
 echo "PATH=\$HOME/anaconda3/bin:\$PATH" >> $HOME/.profile
 echo export PATH >> $HOME/.profile
-
+rm Anaconda2-4.2.0-Linux-x86_64.sh
 
 # Setup Kerberos for Sherlock
 sudo apt-get -y install krb5-user openssh-client # ** interactive **
 sudo curl -o /etc/krb5.conf https://web.stanford.edu/dept/its/support/kerberos/dist/krb5.conf
+
+# Gimp
+sudo apt-get -y install gimp
+sudo apt-add-repository ppa:otto-kesselgulasch/gimp
+sudo apt-get update
+sudo apt-get -y install inkscape
+
+# Ruby
+sudo apt-get update
+sudo apt-get install git-core curl zlib1g-dev build-essential libssl-dev libreadline-dev libyaml-dev libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev libcurl4-openssl-dev python-software-properties libffi-dev
+sudo apt-get install -y libssl-dev libreadline-dev zlib1g-dev
+
+cd
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+exec $SHELL
+
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+echo 'export PATH="$HOME/.rbenv/plugins/ruby-build/bin:$PATH"' >> ~/.bashrc
+exec $SHELL
+rbenv install 2.3.1
+rbenv global 2.3.1
+gem install bundler
+rbenv rehash
+ruby -v
+
+# Rails
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install -y nodejs
+gem install rails -v 4.2.6
+rbenv rehash
+
+# MySQL
+sudo apt-get -y install mysql-server mysql-client libmysqlclient-dev
+
+# Postgres
+sudo sh -c "echo 'deb http://apt.postgresql.org/pub/repos/apt/ xenial-pgdg main' > /etc/apt/sources.list.d/pgdg.list"
+wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+sudo apt-get update
+sudo apt-get -y install postgresql-common
+sudo apt-get -y install postgresql-9.5 libpq-dev
+
+# If you would like to set a password for the user, you can do the following
+sudo -u postgres createuser vanessa -s
+sudo -u postgres psql
+#postgres=# \password vanessa
+
+# Jekyll
+gem install jekyll
